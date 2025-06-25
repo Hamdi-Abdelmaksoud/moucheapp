4b91b8cec4cfa71bbb7427f34b26f5006d50c4d7
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2025-06-25 23:08:22 +0100

    draggable card

--- src/components/portrait/Dragable.jsx
+++ src/components/portrait/Dragable.jsx
@@ -1,19 +1,30 @@
 import { DraggableCardContainer, DraggableCardBody } from "../ui/DraggableCard";
 
 export default function Dragable({ pics }) {
+  const cardPosition=['absolute top-15 left-[33%] sm:left-[43%] rotate-[-5deg]',
+    'absolute top-60 left-[16%] rotate-[-5deg] sm:left-[26%]',
+    'absolute top-55 left-[30%] rotate-[5deg] sm:left-[50%]',
+    'absolute top-25 left-[17%] rotate-[-5deg] sm:left-[55%]',
+    'absolute top-35 left-[13%] rotate-[5deg] sm:left-[33%]',
+    'absolute top-40 left-[22%] sm:left-[43%]  ',
+  ]
   return (
-    <div className="flex flex-wrap justify-center gap-6">
+    <div className="w-full h-700 overflow-hidden">
+      <DraggableCardContainer className='relative   h-[600px] w-full items-center justify-center overflow-clip'>
+    <p className="absolute top-[40%] w-full text-center text-5xl font-black text-neutral-400 md:text-7xl dark:text-neutral-800">
+  The fly sees the unseen
+</p>
+
       {pics.map((pic, index) => (
-        <DraggableCardContainer key={index}>
-          <DraggableCardBody>
+          <DraggableCardBody key={index} className={cardPosition[index]}>
             <img
               src={pic.url}
               alt={pic.title}
-              className="w-full h-auto object-cover rounded-md"
+              className="w-[200px] h-[250px] object-cover rounded-md"
             />
           </DraggableCardBody>
-        </DraggableCardContainer>
       ))}
+      </DraggableCardContainer>
     </div>
   );
 }
